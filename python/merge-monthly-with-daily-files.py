f701e5a99cc55fb9689470e4488de7de141d5b51
viper7882 <klwhot7880@yahoo.co.uk>
2021-10-27 12:04:48 +0800

    Added timeline JSON file dumping in merge-monthly-with-daily-files.py.

--- python/merge-monthly-with-daily-files.py
+++ python/merge-monthly-with-daily-files.py
@@ -1,6 +1,12 @@
 import os
+import copy
+import datetime
+import json
 from pathlib import Path
 import pandas as pd
+import dask.dataframe as dd
+
+from pprint import pprint
 
 # Display all columns
 pd.set_option('display.max_columns', None)
@@ -15,6 +21,7 @@ BINANCE_BASE_DIR = "D:/Wecoz/github/binance-public-data/python/data"
 BINANCE_SPOT_MONTHLY_KLINES = BINANCE_BASE_DIR + "/spot/monthly/klines"
 BINANCE_SPOT_DAILY_KLINES = BINANCE_BASE_DIR + "/spot/daily/klines"
 BINANCE_SPOT_MERGED_KLINES = BINANCE_BASE_DIR + "/spot/merged/klines"
+TIMELINE_JSON_FILE_NAME = "timeline.json"
 
 monthly_base_dir = Path(BINANCE_SPOT_MONTHLY_KLINES).resolve()
 daily_base_dir = Path(BINANCE_SPOT_DAILY_KLINES).resolve()
@@ -30,7 +37,26 @@ def datetime_parse_timestamp(time_in_secs):
 binance_columns = ["Open time", "Open", "High", "Low", "Close", "Volume"]
 selected_columns = range(0, 6)
 
-start_dt_list = []
+overall_timeline_dict_template = {
+    'data_min_dt_str'       : None,
+    'data_max_dt_str'       : None,
+    'specific_timeline_dict': None,     # To be filled with list of specific_timeline_dict
+}
+
+specific_timeline_dict_template = {
+    'symbol_id'             : None,
+    'first_row_dt_str'      : None,
+    'total_indices'         : None,
+    'last_row_dt_str'       : None,
+    'open'                  : None,
+}
+
+overall_timeline_dict = copy.deepcopy(overall_timeline_dict_template)
+overall_timeline_dict['specific_timeline_dict'] = []
+
+data_min_dt = datetime.datetime.utcnow()
+data_max_dt = datetime.datetime(1970, 1, 1)
+
 for symbol_id in monthly_symbols:
     monthly_interval_file_path = Path(os.path.join(monthly_base_dir, symbol_id, BINANCE_INTERVAL)).resolve()
     monthly_file_list = [f for f in os.listdir(monthly_interval_file_path) if
@@ -46,50 +72,99 @@ for symbol_id in monthly_symbols:
     # Sort in place
     daily_file_list.sort()
 
-    monthly_file = monthly_file_list[0].replace(".zip", "")
+    monthly_file_name = monthly_file_list[0].replace(".zip", "")
     daily_file = daily_file_list[-1]
     daily_file_name_split = daily_file.split("-{}-".format(BINANCE_INTERVAL))
-    merged_filename = monthly_file + "-until-" + daily_file_name_split[1]
+    merged_filename = monthly_file_name + "-until-" + daily_file_name_split[1]
 
     # # Debugging Use
     # print (merged_filename)
     # break
 
-    df_list = []
-    for monthly_file in monthly_file_list:
-        monthly_single_file_path = Path(os.path.join(monthly_interval_file_path, monthly_file)).resolve()
-
-        # Loading historical tick data
-        df = pd.read_csv(monthly_single_file_path, index_col=0, parse_dates=True, date_parser=datetime_parse_timestamp,
-                         header=None, names=binance_columns, usecols=selected_columns)
-        df_list.append(df)
-
-        # # Debugging Use
-        # break
-
-    for daily_file in daily_file_list:
-        daily_single_file_path = Path(os.path.join(daily_interval_file_path, daily_file)).resolve()
-
-        # Loading historical tick data
-        df = pd.read_csv(daily_single_file_path, index_col=0, parse_dates=True, date_parser=datetime_parse_timestamp,
-                         header=None, names=binance_columns, usecols=selected_columns)
-        df_list.append(df)
-
-        # # Debugging Use
-        # break
-
-    merged_df = pd.concat(df_list, axis=0, ignore_index=False)
-    merged_df = merged_df.sort_index()
-
-    # Create the directory
     merged_file_dir = Path(os.path.join(merge_dir, symbol_id, BINANCE_INTERVAL)).resolve()
-    if not os.path.exists(merged_file_dir):
-        Path(merged_file_dir).mkdir(parents=True, exist_ok=True)
-
     merged_file_path = os.path.join(merged_file_dir, merged_filename)
+    # If merged file path does not exist
+    if not os.path.exists(merged_file_path):
+        df_list = []
+        for monthly_file in monthly_file_list:
+            monthly_single_file_path = Path(os.path.join(monthly_interval_file_path, monthly_file)).resolve()
 
-    merged_df.to_csv(merged_file_path, compression='zip')
-    print ("INFO: Saved {}".format(merged_file_path))
+            # Loading historical tick data
+            df = pd.read_csv(monthly_single_file_path, index_col=0, parse_dates=True,
+                             date_parser=datetime_parse_timestamp, header=None, names=binance_columns,
+                             usecols=selected_columns)
+            df_list.append(df)
+
+            # # Debugging Use
+            # break
+
+        for daily_file in daily_file_list:
+            daily_single_file_path = Path(os.path.join(daily_interval_file_path, daily_file)).resolve()
+
+            # Loading historical tick data
+            df = pd.read_csv(daily_single_file_path, index_col=0, parse_dates=True,
+                             date_parser=datetime_parse_timestamp, header=None, names=binance_columns,
+                             usecols=selected_columns)
+            df_list.append(df)
+
+            # # Debugging Use
+            # break
+
+        merged_df = pd.concat(df_list, axis=0, ignore_index=False)
+        merged_df = merged_df.sort_index()
+
+        # Contents to be stored into JSON file
+        open_price = float(merged_df[binance_columns[1]].iloc[0])
+        first_row_dt = pd.to_datetime(merged_df.iloc[0].name)
+        last_row_dt = pd.to_datetime(merged_df.iloc[-1].name)
+        total_indices = len(merged_df.index)
+
+        # Create the directory
+        if not os.path.exists(merged_file_dir):
+            Path(merged_file_dir).mkdir(parents=True, exist_ok=True)
+
+        merged_df.to_csv(merged_file_path, compression='zip')
+        print ("INFO: Saved {}".format(merged_file_path))
+    # Else merged file path already exist
+    else:
+        print ("INFO: Found {}".format(merged_file_path))
+
+        ddf = dd.read_csv(merged_file_path, blocksize=None)
+        ddf[binance_columns[0]] = dd.to_datetime(ddf[binance_columns[0]])
+
+        # Contents to be stored into JSON file
+        open_price = ddf.loc[0, binance_columns[1]].compute().to_frame().iloc[0][binance_columns[1]]
+        first_row_dt = ddf.loc[0, binance_columns[0]].compute().to_frame().iloc[0][binance_columns[0]]
+        last_row_dt = ddf.loc[len(ddf.index) - 1, binance_columns[0]].compute().to_frame().iloc[0][binance_columns[0]]
+        total_indices = len(ddf.index)
+
+        # pprint(ddf.head())
+        # pprint(first_row_dt.strftime(DEFAULT_DATE_TIME_FORMAT))
+        # pprint(last_row_dt.strftime(DEFAULT_DATE_TIME_FORMAT))
+
+    if first_row_dt < data_min_dt:
+        data_min_dt = first_row_dt
+
+    if first_row_dt > data_max_dt:
+        data_max_dt = first_row_dt
+
+    specific_timeline_dict = copy.deepcopy(specific_timeline_dict_template)
+    specific_timeline_dict['symbol_id'] = symbol_id
+    specific_timeline_dict['open'] = open_price
+    specific_timeline_dict['first_row_dt_str'] = first_row_dt.strftime(DEFAULT_DATE_TIME_FORMAT)
+    specific_timeline_dict['total_indices'] = total_indices
+    specific_timeline_dict['last_row_dt_str'] = last_row_dt.strftime(DEFAULT_DATE_TIME_FORMAT)
+    overall_timeline_dict['specific_timeline_dict'].append(specific_timeline_dict)
 
     # # Debugging Use
     # break
+
+overall_timeline_dict['data_min_dt_str'] = data_min_dt.strftime(DEFAULT_DATE_TIME_FORMAT)
+overall_timeline_dict['data_max_dt_str'] = data_max_dt.strftime(DEFAULT_DATE_TIME_FORMAT)
+
+# pprint(overall_timeline_dict)
+timeline_file_path = os.path.join(merge_dir, TIMELINE_JSON_FILE_NAME)
+
+with open(timeline_file_path, "w") as outfile:
+    json.dump(overall_timeline_dict, outfile)
+    print("INFO: --> Saved {}".format(timeline_file_path))
