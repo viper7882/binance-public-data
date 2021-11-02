ca25e084379c5c04c36daa23bbbdf19b2309a06f
viper7882 <klwhot7880@yahoo.co.uk>
2021-11-02 15:26:57 +0800

    Replaced merged zip file with only merged parquet file with single partition. Added support for more intervals in download_targeted_symbols.bat and merge-monthly-with-daily-files.py. Reduced the date precision to exclude millisecond to save storage space of JSON file.

--- python/merge-monthly-with-daily-files.py
+++ python/merge-monthly-with-daily-files.py
@@ -10,19 +10,30 @@ from time import time as timer
 from pprint import pprint
 
 # Display all columns
+from enums import INTERVALS
+
 pd.set_option('display.max_columns', None)
 
 # Refer to https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
 DEFAULT_DATE_FORMAT = "%Y-%m-%d"
-DEFAULT_TIME_FORMAT = "%H:%M:%S.%f"
-DEFAULT_DATE_TIME_FORMAT = DEFAULT_DATE_FORMAT + " " + DEFAULT_TIME_FORMAT
+TIME_FORMAT_WITH_MS_PRECISION = DEFAULT_TIME_FORMAT = "%H:%M:%S.%f"
+DATE_TIME_FORMAT_WITH_MS_PRECISION = DEFAULT_DATE_TIME_FORMAT = DEFAULT_DATE_FORMAT + " " + DEFAULT_TIME_FORMAT
+MONTHLY_DATE_FORMAT = "%Y-%m"
+TIME_FORMAT_WITH_S_PRECISION = "%H:%M:%S"
+DATE_TIME_FORMAT_WITH_S_PRECISION = DEFAULT_DATE_FORMAT + " " + TIME_FORMAT_WITH_S_PRECISION
 
-BINANCE_INTERVAL = "1m"
+CSV_FILE_NAME_EXTENSION = ".csv"
+JSON_FILE_NAME_EXTENSION = ".json"
+ZIP_FILE_NAME_EXTENSION = ".zip"
+PARQUET_FILE_NAME_EXTENSION = ".parquet"
+
+# WARNING: the list here has to match with -i in download_targeted_symbols.bat
+SELECTED_BINANCE_INTERVALS = [INTERVALS[0], INTERVALS[2], INTERVALS[4], INTERVALS[6]]
 BINANCE_BASE_DIR = "D:/Wecoz/github/binance-public-data/python/data"
 BINANCE_SPOT_MONTHLY_KLINES = BINANCE_BASE_DIR + "/spot/monthly/klines"
 BINANCE_SPOT_DAILY_KLINES = BINANCE_BASE_DIR + "/spot/daily/klines"
 BINANCE_SPOT_MERGED_KLINES = BINANCE_BASE_DIR + "/spot/merged/klines"
-DEFAULT_TIMELINE_JSON_FILE_NAME = "default_timeline.json"
+DEFAULT_TIMELINE_JSON_FILE_NAME_PREFIX = "default_timeline_"
 
 monthly_base_dir = Path(BINANCE_SPOT_MONTHLY_KLINES).resolve()
 daily_base_dir = Path(BINANCE_SPOT_DAILY_KLINES).resolve()
@@ -65,145 +76,126 @@ specific_timeline_dict_template = {
 #     'open_time_dt_str'  : None,
 # }
 
-
-overall_timeline_dict = copy.deepcopy(overall_timeline_dict_template)
-overall_timeline_dict['specific_timeline_dict'] = []
-
-data_min_dt = datetime.datetime.utcnow()
-data_max_dt = datetime.datetime(1970, 1, 1)
-
 start = timer()
-ddf = None
-for symbol_id in monthly_symbols:
-    monthly_interval_file_path = Path(os.path.join(monthly_base_dir, symbol_id, BINANCE_INTERVAL)).resolve()
-    monthly_file_list = [f for f in os.listdir(monthly_interval_file_path) if
-                         os.path.isfile(os.path.join(monthly_interval_file_path, f))]
+for selected_binance_interval in SELECTED_BINANCE_INTERVALS:
+    overall_timeline_dict = copy.deepcopy(overall_timeline_dict_template)
+    overall_timeline_dict['specific_timeline_dict'] = []
 
-    daily_interval_file_path = Path(os.path.join(daily_base_dir, symbol_id, BINANCE_INTERVAL)).resolve()
-    daily_file_list = [f for f in os.listdir(daily_interval_file_path) if
-                       os.path.isfile(os.path.join(daily_interval_file_path, f))]
+    data_min_dt = datetime.datetime.utcnow()
+    data_max_dt = datetime.datetime(1970, 1, 1)
 
-    # Sort in place
-    monthly_file_list.sort()
+    ddf = None
+    for symbol_id in monthly_symbols:
+        monthly_interval_file_path = Path(os.path.join(monthly_base_dir, symbol_id,
+                                                       selected_binance_interval)).resolve()
+        monthly_file_list = [f for f in os.listdir(monthly_interval_file_path) if
+                             f.endswith(ZIP_FILE_NAME_EXTENSION) and
+                             os.path.isfile(os.path.join(monthly_interval_file_path, f))]
 
-    # Sort in place
-    daily_file_list.sort()
+        daily_interval_file_path = Path(os.path.join(daily_base_dir, symbol_id,
+                                                     selected_binance_interval)).resolve()
+        daily_file_list = [f for f in os.listdir(daily_interval_file_path) if
+                           f.endswith(ZIP_FILE_NAME_EXTENSION) and
+                           os.path.isfile(os.path.join(daily_interval_file_path, f))]
 
-    monthly_file_name = monthly_file_list[0].replace(".zip", "")
-    daily_file = daily_file_list[-1]
-    daily_file_name_split = daily_file.split("-{}-".format(BINANCE_INTERVAL))
-    merged_filename = monthly_file_name + "-until-" + daily_file_name_split[1]
+        # Sort in place
+        monthly_file_list.sort()
 
-    # # Debugging Use
-    # print (merged_filename)
-    # break
+        # Sort in place
+        daily_file_list.sort()
 
-    merged_file_dir = Path(os.path.join(merge_dir, symbol_id, BINANCE_INTERVAL)).resolve()
-    merged_file_path = os.path.join(merged_file_dir, merged_filename)
-    # If merged file path does not exist
-    if not os.path.exists(merged_file_path):
-        df_list = []
-        for monthly_file in monthly_file_list:
-            monthly_single_file_path = Path(os.path.join(monthly_interval_file_path, monthly_file)).resolve()
+        monthly_file_name = monthly_file_list[0].replace(PARQUET_FILE_NAME_EXTENSION, "")
+        daily_file = daily_file_list[-1]
+        daily_file_name_split = daily_file.split("-{}-".format(selected_binance_interval))
+        merged_zip_filename = monthly_file_name + "-until-" + daily_file_name_split[1]
+        merged_parquet_filename = merged_zip_filename.replace(ZIP_FILE_NAME_EXTENSION,
+                                                              PARQUET_FILE_NAME_EXTENSION)
 
-            # Loading historical tick data
-            df = pd.read_csv(monthly_single_file_path, index_col=0, parse_dates=True,
-                             date_parser=datetime_parse_timestamp, header=None, names=binance_columns,
-                             usecols=selected_columns)
-            df_list.append(df)
+        # # Debugging Use
+        # print (merged_zip_filename)
+        # break
 
-            # # Debugging Use
-            # break
+        merged_file_dir = Path(os.path.join(merge_dir, symbol_id, selected_binance_interval)).resolve()
+        merged_parquet_file_path = os.path.join(merged_file_dir, merged_parquet_filename)
 
-        for daily_file in daily_file_list:
-            daily_single_file_path = Path(os.path.join(daily_interval_file_path, daily_file)).resolve()
+        # Check if the PARQUET file is absent
+        if not os.path.exists(merged_parquet_file_path):
+            print("INFO: {} file absent".format(merged_parquet_file_path))
+            df_list = []
+            for monthly_file in monthly_file_list:
+                monthly_single_file_path = Path(os.path.join(monthly_interval_file_path,
+                                                             monthly_file)).resolve()
 
-            # Loading historical tick data
-            df = pd.read_csv(daily_single_file_path, index_col=0, parse_dates=True,
-                             date_parser=datetime_parse_timestamp, header=None, names=binance_columns,
-                             usecols=selected_columns)
-            df_list.append(df)
+                # Loading historical tick data
+                df = pd.read_csv(monthly_single_file_path, index_col=0, parse_dates=True,
+                                 date_parser=datetime_parse_timestamp, header=None, names=binance_columns,
+                                 usecols=selected_columns)
+                df_list.append(df)
 
-            # # Debugging Use
-            # break
+                # # Debugging Use
+                # break
 
-        merged_df = pd.concat(df_list, axis=0, ignore_index=False)
-        merged_df = merged_df.sort_index()
+            for daily_file in daily_file_list:
+                daily_single_file_path = Path(os.path.join(daily_interval_file_path, daily_file)).resolve()
 
-        # Contents to be stored into JSON file
-        open_price = float(merged_df[binance_columns[1]].iloc[0])
-        first_row_dt = pd.to_datetime(merged_df.iloc[0].name)
-        last_row_dt = pd.to_datetime(merged_df.iloc[-1].name)
-        total_indices = len(merged_df.index)
+                # Loading historical tick data
+                df = pd.read_csv(daily_single_file_path, index_col=0, parse_dates=True,
+                                 date_parser=datetime_parse_timestamp, header=None, names=binance_columns,
+                                 usecols=selected_columns)
+                df_list.append(df)
 
-        # Create the directory
-        if not os.path.exists(merged_file_dir):
-            Path(merged_file_dir).mkdir(parents=True, exist_ok=True)
+                # # Debugging Use
+                # break
 
-        merged_df.to_csv(merged_file_path, compression='zip')
-        print ("INFO: Saved {}".format(merged_file_path))
-    # Else merged file path already exist
-    else:
-        print ("INFO: Found {}".format(merged_file_path))
+            merged_df = pd.concat(df_list, axis=0, ignore_index=False)
+            merged_df = merged_df.sort_index()
 
-        ddf = dd.read_csv(merged_file_path, blocksize=None)
-        ddf[binance_columns[0]] = dd.to_datetime(ddf[binance_columns[0]])
-        ddf = ddf.set_index(binance_columns[0], sorted=True)
+            # Contents to be stored into JSON file
+            open_price = float(merged_df[binance_columns[1]].iloc[0])
+            first_row_dt = pd.to_datetime(merged_df.iloc[0].name)
+            last_row_dt = pd.to_datetime(merged_df.iloc[-1].name)
+            total_indices = len(merged_df.index)
 
-        # Contents to be stored into JSON file
-        first_row_dt = ddf.head(1).iloc[0].name
-        last_row_dt = ddf.tail(1).iloc[0].name
-        open_price = ddf.head(1)[binance_columns[1]].iloc[0]
-        total_indices = len(ddf.index)
+            # Create the directory
+            if not os.path.exists(merged_file_dir):
+                Path(merged_file_dir).mkdir(parents=True, exist_ok=True)
 
-        # pprint(ddf.head())
-        # pprint(first_row_dt.strftime(DEFAULT_DATE_TIME_FORMAT))
-        # pprint(last_row_dt.strftime(DEFAULT_DATE_TIME_FORMAT))
+            ddf = dd.from_pandas(merged_df, npartitions=1)
+            ddf.to_parquet(merged_parquet_file_path, overwrite=True)
+            print("INFO: Saved {}".format(merged_parquet_file_path))
+        # Else merged file path already exist
+        else:
+            print("INFO: {} present".format(merged_parquet_file_path))
 
-    if first_row_dt < data_min_dt:
-        data_min_dt = first_row_dt
+        if first_row_dt < data_min_dt:
+            data_min_dt = first_row_dt
 
-    if first_row_dt > data_max_dt:
-        data_max_dt = first_row_dt
+        if first_row_dt > data_max_dt:
+            data_max_dt = first_row_dt
 
-    # if ddf is None:
-    #     ddf = dd.from_pandas(merged_df)
-    #
-    # # For row selection, we must select at least one column as column indexer. Hence the "Open" column is selected in
-    # # this case.
-    # df_open = ddf.iloc[:, [0]].compute()
-    #
-    # index_lookup_dict_list = []
-    # for i, open_time_dt_str in enumerate(df_open.index):
-    # #     index_lookup_dict = copy.deepcopy(index_lookup_dict_template)
-    # #     index_lookup_dict['index'] = i
-    # #     index_lookup_dict['open_time_dt_str'] = str(open_time_dt_str)
-    # #     index_lookup_dict_list.append(index_lookup_dict)
-    #
-    #     index_lookup_dict_list.append((str(open_time_dt_str), df_open[binance_columns[1]].iloc[i]))
+        specific_timeline_dict = copy.deepcopy(specific_timeline_dict_template)
+        specific_timeline_dict['symbol_id'] = symbol_id
+        specific_timeline_dict['open'] = open_price
+        specific_timeline_dict['first_row_dt_str'] = first_row_dt.strftime(DATE_TIME_FORMAT_WITH_S_PRECISION)
+        specific_timeline_dict['total_indices'] = total_indices
+        specific_timeline_dict['last_row_dt_str'] = last_row_dt.strftime(DATE_TIME_FORMAT_WITH_S_PRECISION)
+        #specific_timeline_dict['index_lookup'] = index_lookup_dict_list
+        # specific_timeline_dict['opens'] = index_lookup_dict_list
+        overall_timeline_dict['specific_timeline_dict'].append(specific_timeline_dict)
 
-    specific_timeline_dict = copy.deepcopy(specific_timeline_dict_template)
-    specific_timeline_dict['symbol_id'] = symbol_id
-    specific_timeline_dict['open'] = open_price
-    specific_timeline_dict['first_row_dt_str'] = first_row_dt.strftime(DEFAULT_DATE_TIME_FORMAT)
-    specific_timeline_dict['total_indices'] = total_indices
-    specific_timeline_dict['last_row_dt_str'] = last_row_dt.strftime(DEFAULT_DATE_TIME_FORMAT)
-    #specific_timeline_dict['index_lookup'] = index_lookup_dict_list
-    # specific_timeline_dict['opens'] = index_lookup_dict_list
-    overall_timeline_dict['specific_timeline_dict'].append(specific_timeline_dict)
+        # # Debugging Use
+        # break
 
-    # # Debugging Use
-    # break
+    overall_timeline_dict['data_min_dt_str'] = data_min_dt.strftime(DATE_TIME_FORMAT_WITH_S_PRECISION)
+    overall_timeline_dict['data_max_dt_str'] = data_max_dt.strftime(DATE_TIME_FORMAT_WITH_S_PRECISION)
 
-overall_timeline_dict['data_min_dt_str'] = data_min_dt.strftime(DEFAULT_DATE_TIME_FORMAT)
-overall_timeline_dict['data_max_dt_str'] = data_max_dt.strftime(DEFAULT_DATE_TIME_FORMAT)
+    # pprint(overall_timeline_dict)
+    timeline_file_path = os.path.join(merge_dir, DEFAULT_TIMELINE_JSON_FILE_NAME_PREFIX +
+                                      selected_binance_interval + JSON_FILE_NAME_EXTENSION)
 
-# pprint(overall_timeline_dict)
-timeline_file_path = os.path.join(merge_dir, DEFAULT_TIMELINE_JSON_FILE_NAME)
-
-with open(timeline_file_path, "w") as outfile:
-    outfile.write("{}\n".format(json.dumps(overall_timeline_dict, indent=4, sort_keys=True).strip()))
-    print("INFO: --> Saved {}".format(timeline_file_path))
+    with open(timeline_file_path, "w") as outfile:
+        outfile.write("{}\n".format(json.dumps(overall_timeline_dict, indent=4, sort_keys=True).strip()))
+        print("INFO: --> Saved {}".format(timeline_file_path))
 
 hours, minutes, seconds = get_time_diff(start)
 print("Spent: {}:{}:{:.2f}s".format(int(hours), int(minutes), seconds))
